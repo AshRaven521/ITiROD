d5338c5d4fed56a8e5d38b03d58a5259a353908a
AshRaven521 <ivan.kondrashov.92@mail.ru>
2022-05-11 13:23:31 +0300

    some necessary util functions

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,11 @@
-export function isValid(value){
-  return value.length >= 3;
+export function isRegisterValid(name, email, password) {
+  return name.length >= 3 && email.length >= 3 && password.length >= 6;
+}
+
+export function isLoginValid(email, password) {
+  return email.length >= 3 && password.length >= 3;
+}
+
+export function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
