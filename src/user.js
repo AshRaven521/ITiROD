f11c8e9d14c76d92931c73ea75945449ae30c066
AshRaven521 <ivan.kondrashov.92@mail.ru>
2022-05-19 23:28:50 +0300

    some unnecessary

--- src/user.js
+++ src/user.js
@@ -1,27 +1,35 @@
-export class User{
-  static create(user){
-    return fetch("https://itirod-c3fae-default-rtdb.firebaseio.com/users.json", {
+export class User {
+  static async create(user) {
+    const response = await fetch("https://itirod-c3fae-default-rtdb.firebaseio.com/users.json", {
       method: "POST",
       body: JSON.stringify(user),
       headers: {
-        "Content-Type" : "application/json"
-      }
-    })
-      .then(response => response.json())
-      .then(response => {
-        user.id = response.name;
-        return user;
-      })
-      .then(addToLocalStorage)
+        "Content-Type": "application/json",
+      },
+    });
+    const fullResponse = await response.json();
+    user.id = user.name;
+    const user_1 = user;
+    return addToLocalStorage(user_1);
+  }
+
+  static async get() {
+    const response = await fetch(`https://itirod-c3fae-default-rtdb.firebaseio.com/users/data.json`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const testResponse = await response.json();
   }
 }
 
-function addToLocalStorage(user){
+function addToLocalStorage(user) {
   const allUsers = getUsersFromLocalStorage();
   allUsers.push(user);
   localStorage.setItem("users", JSON.stringify(allUsers));
 }
 
-function getUsersFromLocalStorage(){
+function getUsersFromLocalStorage() {
   return JSON.parse(localStorage.getItem("users") || "[]");
 }
