85684c37717bba08f2656934b2bba9b0ebe7e0db
AshRaven521 <ivan.kondrashov.92@mail.ru>
2022-05-19 23:26:49 +0300

    added functon for time random

--- src/utils.js
+++ src/utils.js
@@ -3,9 +3,17 @@ export function isRegisterValid(name, email, password) {
 }
 
 export function isLoginValid(email, password) {
-  return email.length >= 3 && password.length >= 3;
+  return email.length >= 3 && password.length >= 6;
 }
 
 export function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+export function getRandomTime(){
+  const hours = Math.round(Math.random() * 23);
+  const mins = Math.round(Math.random() * 59);
+  const hourFormat = (hours < 10 ? "0" : "");
+  const minFormat = (mins < 10 ? "0" : "");
+  return String(hourFormat + hours + " : " + minFormat + mins);
 }
