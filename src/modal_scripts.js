7229db5e172a7e1ae12426fe3c32f83b0b304726
AshRaven521 <ivan.kondrashov.92@mail.ru>
2022-05-19 23:28:02 +0300

    added history window handler

--- src/modal_scripts.js
+++ src/modal_scripts.js
@@ -1,3 +1,6 @@
+import { getDatabase, ref, onValue } from "firebase/database";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
 //Для кнопок закрытия
 const closeButtons = document.getElementsByClassName("close-button");
 
@@ -8,6 +11,33 @@ const modalButtons = document.getElementsByClassName("open-modal-button");
 [...modalButtons].forEach((item) => {
   item.onclick = function () {
     const modalId = this.getAttribute("data-modal");
+
+    if (modalId === "3") {
+      const historyList = document.getElementById("history-list");
+      historyList.innerHTML = "";
+      const auth = getAuth();
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          const uid = user.uid;
+          const db = getDatabase();
+          const star = ref(db, "tickets");
+          onValue(star, (snapshot) => {
+            snapshot.forEach((childSnap) => {
+              const childData = childSnap.val();
+              if (childData.id === uid) {
+                const ticketHistoryHTML = `
+                  <li>
+                  <a class="history-link" href="#">${childData.departurePlace}-${childData.arrivalPlace} ${childData.price}Br ${childData.departureDate}</a>
+                  </li>
+                  `;
+                historyList.innerHTML += ticketHistoryHTML;
+              }
+            });
+          });
+        }
+      });
+    }
+
     const modalElem = document.querySelector(`.modal[data-modal="${modalId}"]`);
 
     modalElem.classList.add("active");
